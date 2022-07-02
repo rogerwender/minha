c2e7a3ad57b41f5efe6154b121a686b5f05b73dc
rogerwender <58756028+rogerwender@users.noreply.github.com>
2022-07-02 14:05:26 -0300

    Update index.php

--- index.php
+++ index.php
@@ -12,19 +12,87 @@
 <body>
 	<?php echo "<p>Olá Mundo, Código PHP</p>"; ?>
 <nav>
-  <ul class="menu">
-		<li><a href="#">Home</a></li>
-		<li><a href="#">Sobre</a></li>
-	  		<li><a href="#">O que fazemos?</a>
-	         	<ul>
-	                  <li><a href="#">Web Design</a></li>
-	                  <li><a href="#">SEO</a></li>
-	                  <li><a href="#">Design</a></li>
-	       		</ul>
-			</li>
-		<li><a href="#">Links</a></li>
-		<li><a href="#">Contato</a></li>
-</ul>
+  <ul class="container">
+    <li><a href='#'>Home</a></li>
+    <li class='dropdown'>
+      <a href='#'>Pages <i class="fa fa-angle-down"></i></a>
+      <div class='mega-menu'>
+        <div class="container">
+          <div class="item">
+              <h3>Home Page</h3>
+              <ul>
+                <li><a href='#'>Home Page No #1</a></li>
+                <li><a href='#'>Home Page No #2</a></li>
+                <li><a href='#'>Home Page No #3</a></li>
+                <li><a href='#'>Home Page No #4</a></li>
+
+              </ul>
+          </div> <!-- /.item -->
+          <div class="item">
+              <h3>Contact</h3>
+              <ul>
+                <li><a href='#'>Contact Page No #1</a></li>
+                <li><a href='#'>Contact Page No #2</a></li>
+                <li><a href='#'>Contact Page No #3</a></li>
+                <li><a href='#'>Contact Page No #4</a></li>
+                <li><a href='#'>Contact Page No #5</a></li>
+              </ul>
+          </div> <!-- /.item -->
+          <div class="item">
+              <h3>Portfolio</h3>
+              <ul>
+                <li><a href='#'>Portfolio Page No #1</a></li>
+                <li><a href='#'>Portfolio Page No #2</a></li>
+                <li><a href='#'>Portfolio Page No #3</a></li>
+              </ul>
+          </div> <!-- /.item --> 
+          <div class="item">
+             <h3>Custom</h3>
+             <img src="http://placehold.it/250x200">
+          </div> <!-- /.item -->
+        </div><!-- /.container -->
+      </div><!-- /.mega-menu -->
+    </li><!-- /.dropdown -->
+    <li class='dropdown'>
+      <a href='#'>Images <i class="fa fa-angle-down"></i> </a>
+      <div class='mega-menu'>
+        <div class="container">
+          <div class="item">
+              <h3>Image One</h3>
+              <img src="http://placehold.it/250x200">
+          </div> <!-- /.item -->
+          
+          <div class="item">
+              <h3>Image One</h3>
+              <img src="http://placehold.it/250x200">
+          </div> <!-- /.item -->
+          
+          <div class="item">
+              <h3>Image One</h3>
+              <img src="http://placehold.it/250x200">
+          </div> <!-- /.item -->
+          
+          <div class="item">
+              <h3>Image One</h3>
+              <img src="http://placehold.it/250x200">
+          </div> <!-- /.item -->
+          
+          <div class="item">
+              <h3>Image One</h3>
+              <img src="http://placehold.it/250x200">
+          </div> <!-- /.item -->
+          
+          <div class="item">
+              <h3>Image One</h3>
+              <img src="http://placehold.it/250x200">
+          </div> <!-- /.item -->
+          
+        </div><!-- .container -->
+      </div><!-- .mega-menu-->
+    </li><!-- .dropdown -->
+    <li><a href='#'>About</a></li>
+    <li><a href='#'>Conatct</a></li>
+  </ul><!-- .container -->
 </nav>
 </body>
 </html>

da22f100b32c44d6978779497e5cd15f46b2e47f
rogerwender <58756028+rogerwender@users.noreply.github.com>
2022-07-02 12:51:59 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,5 +4,29 @@
  </head>
  <body>
  <?php echo "<p>Olá Mundo, vasto mundo</p>"; ?>
+  <h1>Rozwijane Menu</h1>
+
+<nav class="nav">
+	<ul>
+		<li><a href="#">Start</a>
+    <li><a href="#">O nas</a>
+		<li class="drop"><a href="#">Oferta</a>
+			<ul class="dropdown">
+				<li><a href="#">Oferta 01</a></li>
+				<li><a href="#">Oferta 02</a></li>
+				<li><a href="#">Oferta 03</a></li>
+			</ul>
+		</li>
+		<li><a href="#">Aktualności</a>
+		<li><a href="#">Kontakt</a>
+	</ul>
+</nav>
+
+<footer>
+    <p class="main">
+        2017 © nayma.pl | Wszystkie prawa zastrzeżone | Design by <a href="https://nayma.pl" target="_blank">nayma.pl</a>
+    </p>
+</footer>
+  
  </body>
 </html>

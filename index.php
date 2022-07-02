4f236a8d5d18b8a9dd2a7e77dc8882f885b1a5ba
rogerwender <58756028+rogerwender@users.noreply.github.com>
2022-07-02 12:56:14 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,32 +1,30 @@
-<html>
- <head>
-  <title>Teste PHP</title>
- </head>
- <body>
- <?php echo "<p>Olá Mundo, vasto mundo</p>"; ?>
-  <h1>Rozwijane Menu</h1>
-
-<nav class="nav">
-	<ul>
-		<li><a href="#">Start</a>
-    <li><a href="#">O nas</a>
-		<li class="drop"><a href="#">Oferta</a>
-			<ul class="dropdown">
-				<li><a href="#">Oferta 01</a></li>
-				<li><a href="#">Oferta 02</a></li>
-				<li><a href="#">Oferta 03</a></li>
-			</ul>
-		</li>
-		<li><a href="#">Aktualności</a>
-		<li><a href="#">Kontakt</a>
-	</ul>
+<!DOCTYPE HTML>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+    <title>Menu Dropdown Horizontal - Linha de Código</title>
+     <!-- Aqui chamamos o nosso arquivo css externo -->
+    <link rel="stylesheet" type="text/css"  href="estilo.css" />
+    <!--[if lte IE 8]>
+ <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+ <![endif]-->
+</head>
+<body>
+	<?php echo "<p>Olá Mundo, Código PHP</p>"; ?>
+<nav>
+  <ul class="menu">
+		<li><a href="#">Home</a></li>
+		<li><a href="#">Sobre</a></li>
+	  		<li><a href="#">O que fazemos?</a>
+	         	<ul>
+	                  <li><a href="#">Web Design</a></li>
+	                  <li><a href="#">SEO</a></li>
+	                  <li><a href="#">Design</a></li>
+	       		</ul>
+			</li>
+		<li><a href="#">Links</a></li>
+		<li><a href="#">Contato</a></li>
+</ul>
 </nav>
-
-<footer>
-    <p class="main">
-        2017 © nayma.pl | Wszystkie prawa zastrzeżone | Design by <a href="https://nayma.pl" target="_blank">nayma.pl</a>
-    </p>
-</footer>
-  
- </body>
+</body>
 </html>

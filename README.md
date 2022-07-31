8f2f30dd3671d4672c3af2d411727125b53b7b04
rogerwender <58756028+rogerwender@users.noreply.github.com>
2022-07-31 13:40:57 -0300

    NovaPagina

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# minha
-minha aplicacao
+[![](https://img.shields.io/badge/python-3.7+-blue.svg)](https://www.python.org/downloads/release/python-365/) [![GPLv3 license](https://img.shields.io/badge/License-GPLv3-blue.svg)](http://perso.crans.org/besson/LICENSE.html) 
+
+
+# Análise Preditiva
+* [Prevendo a fadiga de Funcionários](https://github.com/rogerwender/CientistaDeDados/blob/24f9fd1b75b2866818387be471e9e00038d64103/Prevendo_Fadiga_de_Funcion%C3%A1rios.ipynb)
+* [Sistema de recomendação de Livros](https://github.com/rogerwender/CienciaDados/blob/main/Sistema_de_Recomenda%C3%A7%C3%A3o_de_Livros.ipynb)
+
+# Tutoriais em Python:
+
+* [Comandos básicos](https://github.com/rogerwender/CientistaDeDados/blob/main/python/1-ConceitosBasicos.ipynb)
+* [Laços de repetição](https://github.com/rogerwender/CientistaDeDados/blob/main/python/2-LacosRepeticao.ipynb)
+* [Listas](https://github.com/rogerwender/CientistaDeDados/blob/main/python/3-Listas-.ipynb)
+* [Dicionários](https://github.com/rogerwender/CientistaDeDados/blob/main/python/4-Dicionarios.ipynb)
+* [Funções](https://github.com/rogerwender/CientistaDeDados/blob/main/python/5-Fun%C3%A7%C3%B5es.ipynb)
+* [Manipulando arquivos]( https://github.com/rogerwender/CientistaDeDados/blob/main/python/6-ManipulandoArquivos.ipynb)
+* [Tradutor em Python](https://github.com/rogerwender/DataScience/blob/main/TradutorEmPython.ipynb)
+* [Web Scraping](https://github.com/rogerwender/DataScience/blob/main/WebScraping.ipynb)
+* [Operadora Telefone](https://github.com/rogerwender/DataScience/blob/main/OperadoraTelefone.ipynb)
+* [Gerar QRCode](https://github.com/rogerwender/DataScience/blob/main/GerarQRCode.ipynb)
+
+
+# Meu LinkedIn
+* [RogerioWender](https://www.linkedin.com/in/rogerio-wender-4b9179193/)
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="pt-br">
 <head>
   <meta charset="UTF-8">
-    <title>Menu Dropdown Horizontal - Linha de Código</title>
+    <title>Meus projetos em Python - Rogério Wender</title>
      <!-- Aqui chamamos o nosso arquivo css externo -->
     <link rel="stylesheet" type="text/css"  href="estilo.css" />
     <!--[if lte IE 8]>
@@ -10,32 +10,33 @@
  <![endif]-->
 </head>
 <body>
-	<?php echo "<p>Olá Mundo vasto mundo, Código PHP</p>"; ?>
 <nav>
   <ul class="container">
-    <li><a href='#'>Pagina inicial</a></li>
+    <li><a href='#'>Home</a>
+	    
+    </li>
     <li class='dropdown'>
-      <a href='#'>Pages <i class="fa fa-angle-down"></i></a>
+      <a href='#'>Meus Projetos <i class="fa fa-angle-down"></i></a>
       <div class='mega-menu'>
         <div class="container">
           <div class="item">
-              <h3>Home Page</h3>
+              <h3>Projetos em Python</h3>
               <ul>
-                <li><a href='#'>Home Page No #1</a></li>
-                <li><a href='#'>Home Page No #2</a></li>
-                <li><a href='#'>Home Page No #3</a></li>
-                <li><a href='#'>Home Page No #4</a></li>
+                <li><a href='https://github.com/rogerwender/DataScience/blob/main/Prevendo_Fadiga_de_Funcionarios.ipynb'>Prevendo a fadiga de funcionarios</a></li>
+                <li><a href='https://github.com/rogerwender/DataScience/blob/main/OperadoraTelefone.ipynb'>Verificar Operadora de telefone</a></li>
+                <li><a href='https://github.com/rogerwender/DataScience/blob/main/GerarQRCode.ipynb'>GerarQRCode</a></li>
+                <li><a href='https://github.com/rogerwender/DataScience/blob/main/Sistema_de_Recomendacao_de_Livros.ipynb'>Sistema de Recomendacao de Livros</a></li>
 
               </ul>
           </div> <!-- /.item -->
           <div class="item">
-              <h3>Contatos</h3>
+              <h3>Projetos em Python </h3>
               <ul>
-                <li><a href='#'>Contatos Page No #1</a></li>
-                <li><a href='#'>Contatos Page No #2</a></li>
-                <li><a href='#'>Contatos Page No #3</a></li>
-                <li><a href='#'>Contatos Page No #4</a></li>
-                <li><a href='#'>Contatos Page No #5</a></li>
+                <li><a href='https://github.com/rogerwender/DataScience/blob/main/TradutorEmPython.ipynb'>Tradutor em Python</a></li>
+                <li><a href='https://github.com/rogerwender/DataScience/blob/main/WebScraping.ipynb'>Web Scraping</a></li>
+                <li><a href='#'>Contact Page No #3</a></li>
+                <li><a href='#'>Contact Page No #4</a></li>
+                <li><a href='#'>Contact Page No #5</a></li>
               </ul>
           </div> <!-- /.item -->
           <div class="item">
@@ -54,48 +55,51 @@
       </div><!-- /.mega-menu -->
     </li><!-- /.dropdown -->
     <li class='dropdown'>
-      <a href='#'>Images <i class="fa fa-angle-down"></i> </a>
+      <a href='#'>Imagens <i class="fa fa-angle-down"></i> </a>
       <div class='mega-menu'>
         <div class="container">
           <div class="item">
-              <h3>Image One</h3>
+              <h3>Imagem 01</h3>
               <img src="http://placehold.it/250x200">
           </div> <!-- /.item -->
           
           <div class="item">
-              <h3>Image One</h3>
+              <h3>Imagem 02</h3>
               <img src="http://placehold.it/250x200">
           </div> <!-- /.item -->
           
           <div class="item">
-              <h3>Image One</h3>
+              <h3>Imagem 03</h3>
               <img src="http://placehold.it/250x200">
           </div> <!-- /.item -->
           
           <div class="item">
-              <h3>Image One</h3>
+              <h3>Imagem 04</h3>
               <img src="http://placehold.it/250x200">
           </div> <!-- /.item -->
           
           <div class="item">
-              <h3>Image One</h3>
+              <h3>Imagem 05</h3>
               <img src="http://placehold.it/250x200">
           </div> <!-- /.item -->
           
           <div class="item">
-              <h3>Image One</h3>
+              <h3>Imagem 06</h3>
               <img src="http://placehold.it/250x200">
           </div> <!-- /.item -->
           
         </div><!-- .container -->
       </div><!-- .mega-menu-->
     </li><!-- .dropdown -->
-    <li><a href='#'>About</a></li>
-    <li><a href='#'>Conatct</a></li>
+    <li><a href='#'>Sobre</a></li>
+    <li><a href='#'>Contatos</a></li>
   </ul><!-- .container -->
 </nav>
 	
-
+<br><br><br><br><br><br>
+	<h1 align="center">
+	<img src='PaginaEmConstrucao.png'>
+	</h1>
 
 </body>
 </html>
